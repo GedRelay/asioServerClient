a9f60d8732c5309fa9cbc41058385d57b7977ba6
gedrelay <2515858428@qq.com>
2025-03-15 19:55:25 +0800

    B-server

---
+++ server/B-server/src/CServer.cpp
@@ -0,0 +1,28 @@
+#include "../include/CServer.h"
+
+
+CServer::CServer(boost::asio::io_context &io_context, unsigned short port):
+    _io_context(io_context),
+    _acceptor(io_context, boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), port)){
+    WaitAccept();
+}
+
+
+// 异步等待连接
+void CServer::WaitAccept(){
+    CSession * session = new CSession(_io_context);
+    // 注册连接事件
+    _acceptor.async_accept(
+        session->GetSocket(), 
+        [this, session](const boost::system::error_code &error){
+            if(!error){
+                std::cout << "new session connected" << std::endl;
+                session->Start();
+                WaitAccept();
+            }
+            else{
+                delete session;
+            }
+        }
+    );
+}
